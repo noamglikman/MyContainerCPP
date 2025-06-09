049c5f0ca40d073724ec0cf1484f227483ea2835
Noam Glickman <noamglikman1@gmail.com>
2025-06-09 23:29:55 +0300

    2

--- demo.cpp
+++ demo.cpp
@@ -16,32 +16,33 @@ int main() {
     container.add(2);
 
     std::cout << "Size of container: " << container.size() << std::endl;
-
+    std::cout << "Ascending order: ";
     for (auto it = container.begin_ascending_order(); it != container.end_ascending_order(); ++it) {
         std::cout << *it << ' ';   // 1 2 6 7 15
     }
     std::cout << std::endl;
 
+    std::cout << "Descending order: ";
     for (auto it = container.begin_descending_order(); it != container.end_descending_order(); ++it) {
         std::cout << *it << ' ';  // 15 7 6 2 1
     }
     std::cout << std::endl;
-
+    std::cout << "Side cross order: ";
     for (auto it = container.begin_side_cross_order(); it != container.end_side_cross_order(); ++it) {
         std::cout << *it << ' ';  // 1 15 2 7 6
     }
     std::cout << std::endl;
-
+    std::cout << "reverse order: ";
     for (auto it = container.begin_reverse_order(); it != container.end_reverse_order(); ++it) {
         std::cout << *it << ' ';  // 2 1 6 15 7
     }
     std::cout << std::endl;
-
+    std::cout << "begin order: ";
     for (auto it = container.begin_order(); it != container.end_order(); ++it) {
         std::cout << *it << ' ';  // 7 15 6 1 2
     }
     std::cout << std::endl;
-
+    std::cout << "middle order: ";
     for (auto it = container.begin_middle_out_order(); it != container.end_middle_out_order(); ++it) {
         std::cout << *it << ' ';  // 6 15 1 7 2
     }
